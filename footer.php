47251a8b8e40cb4e6f05cc258a15bcfb321f2971
Joe Fearnley <joe.fearnley@gmail.com>
2010-11-23 10:17:14 -0800

    Removed analytics stuff.

--- footer.php
+++ footer.php
@@ -32,17 +32,5 @@
 	<br class="clear" />
 
 </div><!-- end of #footer -->
-
-	<script type="text/javascript">
-		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-	</script>
-	<script type="text/javascript">
-		try {
-			var pageTracker = _gat._getTracker("UA-6368887-1");
-			pageTracker._trackPageview();
-		} catch(err) {}
-	</script>
-
 </body>
 </html>
